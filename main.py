713b5fefe1d0a62d44041b789b746ccdac19f310
Parneet0623 <40465006+Parneet0623@users.noreply.github.com>
2024-09-16 10:13:28 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+### Integrate HTMP With Flask
+### HTTP verb GET and POST
+### Building Url Dynamically
+### Variable Rules and URL Building
+
+from flask import Flask,redirect, url_for, render_template,request
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+
+@app.route('/success/<int:score>')
+def success(score):
+    res=""
+    if score>=50:
+        res="PASS"
+    else:
+        res="Fail"
+    return render_template('result.html', result=res)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "Person has failed and marks is " + str(score)
+
+##Results checker
+@app.route('/results/<int:marks>')
+def results(marks):
+    result=""
+    if marks<50:
+        result="fail"
+    else:
+        result="success"
+    return redirect(url_for(result,score=marks))
+
+##result checker submit HTML Page
+@app.route('/submit', methods = ['POST', 'GET'])
+def submit():
+    total_score=0
+    if request.method=="POST":
+        science=float(request.form['Science'])
+        maths=float(request.form['Maths'])
+        C=float(request.form['C'])
+        data_science=float(request.form['DataScience'])
+        total_score=(science+maths+C+data_science)/4
+    res=""
+    if total_score>=50:
+        res="success"
+    else:
+        res="fail"
+    return redirect(url_for('success',score=total_score))
+
+if __name__=='__main__':
+    app.run(debug = True)
+
+
